c3abba2126bc01eee44f938822c985c7f54ce064
Yusuke Yachi <yusuke.yachi@gmail.com>
2015-07-24 16:28:35 +0900

    find

---
+++ lib/orochi_for_medusa/commands/find.rb
@@ -0,0 +1,70 @@
+require 'orochi_for_medusa/cui'
+module OrochiForMedusa::Commands
+	class Find < OrochiForMedusa::Cui
+		def option_parser
+			opts = OptionParser.new do |opt|
+				opt.banner = <<-"EOS".unindent
+					NAME
+						#{File.basename($0, '.*')} - Search Medusa by keyword
+
+					SYNOPSIS
+						#{File.basename($0, '.*')} [options] keyword
+
+					DESCRIPTION
+						Search Medusa by keyword and return Medusa-ID.  To obtain name of
+						Medusa-ID, use orochi-name.
+
+					EXAMPLE
+    					DOS> ruby -S orochi-find maagnetite
+    					DOS> ruby -S orochi-find maagnetite | ruby -S orochi-name
+
+					SEE ALSO
+    					orochi-name
+    					http://dream.misasa.okayama-u.ac.jp
+
+					IMPLEMENTATION
+						Orochi, version 9
+						Copyright (C) 2015 Okayama University
+						License GPLv3+: GNU GPL version 3 or later
+
+					OPTIONS
+				EOS
+				opt.on("-v", "--[no-]verbose", "Run verbosely") {|v| OPTS[:verbose] = v}				
+ 				opt.on("--name", "Display name") {|v| OPTS[:name] = v}
+			end
+			opts
+		end
+
+		def execute
+			ids = argv.clone
+			
+			if argv.length < 1
+  				while answer = stdin.gets do
+    				find(answer.chomp)
+  				end
+			elsif argv.length > 1
+				puts opts
+			else
+				find(argv[0])
+			end
+		end	
+
+
+		def find(keyword)
+		  page = 1
+		  while true
+		    objs = Record.find(:all, :params =>{:q => {:name_or_global_id_cont => keyword}, :page => page})
+		    break if objs.size == 0
+		    objs.each do |obj|
+		      if OPTS[:name]
+		        stdout.puts obj.name
+		      else
+		        stdout.puts obj.global_id
+		      end
+		    end
+		    page += 1
+		  end
+		end
+
+	end
+end
---
+++ spec/orochi_for_medusa/commands/find_spec.rb
@@ -0,0 +1,67 @@
+require 'spec_helper'
+require 'orochi_for_medusa/commands/find'
+include MedusaRestClient
+
+module OrochiForMedusa::Commands
+	describe Find do
+		let(:cui) { Find.new(args, :stdin => stdin, :stdout => stdout, :stderr => stderr, :program_name => 'orochi-find') }
+		let(:args){ [] }
+		let(:stdout){ Output.new }
+		let(:stderr){ Output.new }
+		let(:stdin){ double('stdin').as_null_object }
+		describe "show_help", :show_help => true do
+			it { 
+				puts "-" * 5 + " help start" + "-" * 5
+				puts cui.opts 
+				puts "-" * 5 + " help end" + "-" * 5
+			}
+		end
+
+		describe "parse_options" do
+			subject { cui.parse_options }
+
+			describe "with -v" do
+				let(:args){ ["-v"] }
+				it { 
+					subject
+					expect(cui.options).to include(:verbose => true)
+				}
+			end
+
+
+		end
+
+		describe "execute" do		
+			subject { cui.execute }
+			before do
+				cui.parse_options
+			end
+
+
+			describe "with id" do
+				let(:args){ [id_1] }
+				let(:id_1){ '0000000-001'}
+				it {
+					expect(cui).to receive(:find).with(id_1)
+					subject
+				}
+
+			end
+		end
+
+		describe "find" do
+			subject { cui.find keyword }
+			let(:keyword){ 'hello'}
+			let(:objs){ [obj_1] }
+			let(:obj_1) { double("record", :name => 'dummy', :global_id => "0000-001").as_null_object }
+
+			before do
+				allow(Record).to receive(:find).and_return([])
+			end
+			it {
+				subject
+			}
+		end
+
+	end
+end
